5ef713b52a8ec5e2565083252aede26bd09935f2
rgayatri <rgayatri@lbl.gov>
2020-09-02 09:17:12 -0700

    First commit for the Kokkos version of GPP. Uses Kokkos:complex.

--- GPP/gpp.cpp
+++ GPP/gpp.cpp
@@ -55,252 +55,153 @@ form.
 */
 
 #include "../common/Defines.h"
+#include <Kokkos_Core.hpp>
 
-#if defined(_OPENACC)
-inline void init_structs(size_t number_bands, size_t ngpown, size_t ncouls,
-                         ARRAY2D &aqsmtemp, ARRAY2D &aqsntemp,
-                         ARRAY2D &I_eps_array, ARRAY2D &wtilde_array,
-                         ARRAY1D_DataType &vcoul, ARRAY1D_int &inv_igp_index,
-                         ARRAY1D_int &indinv, ARRAY1D_DataType &wx_array) {
-  const DataType dw = 1;
-  const DataType e_lk = 10;
-  const DataType to1 = 1e-6;
-  const DataType limittwo = pow(0.5, 2);
-  const DataType e_n1kq = 6.0;
-  CustomComplex<DataType> expr(0.5, 0.5);
-
-#pragma acc enter data copyin(aqsmtemp, aqsntemp, vcoul, inv_igp_index,        \
-                              indinv, I_eps_array, wtilde_array, wx_array)
-
-#pragma acc enter data create(                                                 \
-    aqsmtemp.dptr [0:aqsmtemp.size], vcoul.dptr [0:vcoul.size],                \
-    inv_igp_index.dptr [0:inv_igp_index.size], indinv.dptr [0:indinv.size],    \
-    aqsntemp.dptr [0:aqsntemp.size], I_eps_array.dptr [0:I_eps_array.size],    \
-    wx_array.dptr [nstart:nend], wtilde_array.dptr [0:wtilde_array.size])
-
-#pragma acc parallel loop present(aqsmtemp, aqsntemp)
-  for (int i = 0; i < number_bands; i++)
-    for (int j = 0; j < ncouls; j++) {
-      aqsmtemp(i, j) = CustomComplex<DataType>(0.5, 0.5);
-      aqsntemp(i, j) = CustomComplex<DataType>(0.5, 0.5);
-    }
-
-#pragma acc parallel loop copyin(expr) present(I_eps_array, wtilde_array)
-  for (int i = 0; i < ngpown; i++)
-    for (int j = 0; j < ncouls; j++) {
-      I_eps_array(i, j) = expr;
-      wtilde_array(i, j) = expr;
-    }
-
-#pragma acc parallel loop present(vcoul)
-  for (int i = 0; i < ncouls; i++)
-    vcoul(i) = 1.0;
-
-#pragma acc parallel loop present(inv_igp_index)
-  for (int ig = 0; ig < ngpown; ++ig)
-    inv_igp_index(ig) = (ig + 1) * ncouls / ngpown;
-
-#pragma acc parallel loop present(indinv)
-  for (int ig = 0; ig < ncouls; ++ig)
-    indinv(ig) = ig;
-  indinv(ncouls) = ncouls - 1;
-
-#pragma acc parallel loop present(wx_array)
-  for (int iw = nstart; iw < nend; ++iw) {
-    wx_array(iw) = e_lk - e_n1kq + dw * ((iw + 1) - 2);
-    if (wx_array(iw) < to1)
-      wx_array(iw) = to1;
-  }
-}
-#endif
-
-inline void correntess(CustomComplex<DataType> result) {
+inline void correntess(KokkosComplex result) {
   double re_diff, im_diff;
-#if defined(_OPENMP) || defined(_OPENACC)
-  re_diff = result.get_real() - -264241149.849658;
-  im_diff = result.get_imag() - 1321205773.349384;
-#else
-  re_diff = result.get_real() - -264241220.914570;
-  im_diff = result.get_imag() - 1321205332.084101;
-#endif
+  re_diff = result.real() - -264241156.144946;
+  im_diff = result.imag() - 1321205754.194689;
 
   if (re_diff < 0.001 && im_diff < 0.001)
     printf("\n!!!! SUCCESS - !!!! Correctness test passed :-D :-D\n\n");
   else
-    printf("\n!!!! FAILURE - Correctness test failed :-( :-(  \n");
+    printf("\n!!!! FAILURE - Correctness test failed :-( :-(.\n You are away "
+           "by (%f,%f) \n",
+           re_diff, im_diff);
 }
 
 int main(int argc, char **argv) {
 
-#if defined(OPENMP_TARGET)
-  cout << "\n ************OpenMP 4.5**********\n" << endl;
-#elif defined(_OPENMP)
-  cout << "\n ************OpenMP 3.0**********\n" << endl;
-#elif defined(_OPENACC)
-  cout << "\n ************OpenACC  **********\n" << endl;
-#endif
-
-  int number_bands = 0, nvband = 0, ncouls = 0, nodes_per_group = 0;
-  int npes = 1;
-  if (argc == 1) {
-    number_bands = 512;
-    nvband = 2;
-    ncouls = 32768;
-    nodes_per_group = 20;
-  } else if (argc == 5) {
-    number_bands = atoi(argv[1]);
-    nvband = atoi(argv[2]);
-    ncouls = atoi(argv[3]);
-    nodes_per_group = atoi(argv[4]);
-  } else {
-    std::cout << "The correct form of input is : " << endl;
-    std::cout << " ./a.out <number_bands> <number_valence_bands> "
-                 "<number_plane_waves> <nodes_per_mpi_group> "
-              << endl;
-    exit(0);
-  }
-  int ngpown = ncouls / (nodes_per_group * npes);
-
-  // Constants that will be used later
-  const DataType e_lk = 10;
-  const DataType dw = 1;
-  const DataType to1 = 1e-6;
-  const DataType limittwo = pow(0.5, 2);
-  const DataType e_n1kq = 6.0;
-
-  // Using time point and system_clock
-  time_point<system_clock> start, end, k_start, k_end;
-  start = system_clock::now();
-  double elapsedKernelTimer;
-
-  // OpenMP Printing of threads on Host and Device
-#if defined(_OPENMP)
-  int numThreads = 1, numTeams = 2;
-#if defined(OPENMP_TARGET)
-#pragma omp target teams map(tofrom : numTeams, numThreads) \
-  shared(numTeams)
+  Kokkos::initialize(argc, argv);
   {
-    int tid = omp_get_team_num();
-    if (tid == 0) {
-      numTeams = omp_get_num_teams();
-#pragma omp parallel
-      {
-        int ttid = omp_get_thread_num();
-        if (ttid == 0)
-          numThreads = omp_get_num_threads();
+    int number_bands = 0, nvband = 0, ncouls = 0, nodes_per_group = 0;
+    int npes = 1;
+    if (argc == 1) {
+      number_bands = 512;
+      nvband = 2;
+      ncouls = 32768;
+      nodes_per_group = 20;
+    } else if (argc == 5) {
+      number_bands = atoi(argv[1]);
+      nvband = atoi(argv[2]);
+      ncouls = atoi(argv[3]);
+      nodes_per_group = atoi(argv[4]);
+    } else {
+      std::cout << "The correct form of input is : "
+                << "\n";
+      std::cout << " ./a.out <number_bands> <number_valence_bands> "
+                   "<number_plane_waves> <nodes_per_mpi_group> "
+                << "\n";
+      exit(0);
+    }
+    int ngpown = ncouls / (nodes_per_group * npes);
+
+    // Constants that will be used later
+    const DataType e_lk = 10;
+    const DataType dw = 1;
+    const DataType to1 = 1e-6;
+    const DataType limittwo = pow(0.5, 2);
+    const DataType e_n1kq = 6.0;
+
+    // Using time point and system_clock
+    time_point<system_clock> start, end, k_start, k_end;
+    start = system_clock::now();
+    double elapsedKernelTimer;
+
+    // Printing out the params passed.
+    std::cout << "Sizeof(KokkosComplex) = " << sizeof(KokkosComplex) << " bytes"
+              << "\n";
+    std::cout << "number_bands = " << number_bands << "\t nvband = " << nvband
+              << "\t ncouls = " << ncouls
+              << "\t nodes_per_group  = " << nodes_per_group
+              << "\t ngpown = " << ngpown << "\t nend = " << nend
+              << "\t nstart = " << nstart << "\n";
+
+    size_t memFootPrint = 0.00;
+
+    // ALLOCATE statements from fortran gppkernel.
+    ComplexStruct achtemp;
+    memFootPrint += (nend - nstart) * sizeof(KokkosComplex);
+
+    ARRAY2D aqsmtemp("aqsmtemp", number_bands, ncouls);
+    ARRAY2D aqsntemp("aqsntemp", number_bands, ncouls);
+    memFootPrint += 2 * (number_bands * ncouls) * sizeof(KokkosComplex);
+
+    ARRAY2D I_eps_array("I_eps_array", ngpown, ncouls);
+    ARRAY2D wtilde_array("wtilde_array", ngpown, ncouls);
+    memFootPrint += 2 * (ngpown * ncouls) * sizeof(KokkosComplex);
+
+    ARRAY1D_DataType vcoul("vcoul", ncouls);
+    memFootPrint += ncouls * sizeof(DataType);
+
+    ARRAY1D_int inv_igp_index("inv_igp_index", ngpown);
+    ARRAY1D_int indinv("indinv", ncouls + 1);
+
+    memFootPrint += ngpown * sizeof(int);
+    memFootPrint += (ncouls + 1) * sizeof(int);
+
+    ARRAY1D_DataType wx_array("wx_array", nend - nstart);
+    memFootPrint += 3 * (nend - nstart) * sizeof(DataType);
+
+    // Print Memory Foot print
+    std::cout << "Memory Foot Print = " << memFootPrint / pow(1024, 3) << " GBs"
+              << "\n";
+
+    KokkosComplex expr(.5, .5);
+    for (int i = 0; i < number_bands; i++)
+      for (int j = 0; j < ncouls; j++) {
+        aqsmtemp.h_view(i, j) = expr;
+        aqsntemp.h_view(i, j) = expr;
       }
-    }
-  }
-  std::cout << "Number of OpenMP Teams = " << numTeams << std::endl;
-  std::cout << "Number of OpenMP DEVICE Threads = " << numThreads << std::endl;
-#else
-#pragma omp parallel shared(numThreads)
-  {
-    int tid = omp_get_thread_num();
-    if (tid == 0)
-      numThreads = omp_get_num_threads();
-  }
-  std::cout << "Number of OpenMP Threads = " << numThreads << endl;
-#endif
-#endif
 
+    for (int i = 0; i < ngpown; i++)
+      for (int j = 0; j < ncouls; j++) {
+        I_eps_array.h_view(i, j) = expr;
+        wtilde_array.h_view(i, j) = expr;
+      }
 
-  // Printing out the params passed.
-  std::cout << "Sizeof(CustomComplex<DataType> = "
-            << sizeof(CustomComplex<DataType>) << " bytes" << std::endl;
-  std::cout << "number_bands = " << number_bands << "\t nvband = " << nvband
-            << "\t ncouls = " << ncouls
-            << "\t nodes_per_group  = " << nodes_per_group
-            << "\t ngpown = " << ngpown << "\t nend = " << nend
-            << "\t nstart = " << nstart << endl;
+    for (int i = 0; i < ncouls; i++)
+      vcoul.h_view(i) = 1.0;
 
-  size_t memFootPrint = 0.00;
+    for (int ig = 0; ig < ngpown; ++ig)
+      inv_igp_index.h_view(ig) = (ig + 1) * ncouls / ngpown;
 
-  // ALLOCATE statements from fortran gppkernel.
-  ARRAY1D achtemp(nend - nstart);
-  memFootPrint += (nend - nstart) * sizeof(CustomComplex<DataType>);
+    // Do not know yet what this array represents
+    for (int ig = 0; ig < ncouls; ++ig)
+      indinv.h_view(ig) = ig;
+    indinv.h_view(ncouls) = ncouls - 1;
 
-  ARRAY2D aqsmtemp(number_bands, ncouls);
-  ARRAY2D aqsntemp(number_bands, ncouls);
-  memFootPrint += 2 * (number_bands * ncouls) * sizeof(CustomComplex<DataType>);
-
-  ARRAY2D I_eps_array(ngpown, ncouls);
-  ARRAY2D wtilde_array(ngpown, ncouls);
-  memFootPrint += 2 * (ngpown * ncouls) * sizeof(CustomComplex<DataType>);
-
-  ARRAY1D_DataType vcoul(ncouls);
-  memFootPrint += ncouls * sizeof(DataType);
-
-  ARRAY1D_int inv_igp_index(ngpown);
-  ARRAY1D_int indinv(ncouls + 1);
-  memFootPrint += ngpown * sizeof(int);
-  memFootPrint += (ncouls + 1) * sizeof(int);
-
-  ARRAY1D_DataType wx_array(nend - nstart);
-  memFootPrint += 3 * (nend - nstart) * sizeof(DataType);
-
-  // Print Memory Foot print
-  cout << "Memory Foot Print = " << memFootPrint / pow(1024, 3) << " GBs"
-       << endl;
-
-#if !defined(_OPENACC)
-  CustomComplex<DataType> expr(.5, .5);
-  for (int i = 0; i < number_bands; i++)
-    for (int j = 0; j < ncouls; j++) {
-      aqsmtemp(i, j) = expr;
-      aqsntemp(i, j) = expr;
+    for (int iw = nstart; iw < nend; ++iw) {
+      wx_array.h_view(iw) = e_lk - e_n1kq + dw * ((iw + 1) - 2);
+      if (wx_array.h_view(iw) < to1)
+        wx_array.h_view(iw) = to1;
     }
 
-  for (int i = 0; i < ngpown; i++)
-    for (int j = 0; j < ncouls; j++) {
-      I_eps_array(i, j) = expr;
-      wtilde_array(i, j) = expr;
-    }
+    k_start = system_clock::now();
+    noflagOCC_solver(number_bands, ngpown, ncouls, inv_igp_index, indinv,
+                     wx_array, wtilde_array, aqsmtemp, aqsntemp, I_eps_array,
+                     vcoul, achtemp);
 
-  for (int i = 0; i < ncouls; i++)
-    vcoul(i) = 1.0;
+    k_end = system_clock::now();
+    duration<double> elapsed = k_end - k_start;
+    elapsedKernelTimer = elapsed.count();
 
-  for (int ig = 0; ig < ngpown; ++ig)
-    inv_igp_index(ig) = (ig + 1) * ncouls / ngpown;
+    // Check for correctness
+    correntess(achtemp.val[0]);
+    printf("\n Final achtemp\n");
+    printf("(%f,%f) \n", achtemp.val[0].real(), achtemp.val[0].imag());
 
-  // Do not know yet what this array represents
-  for (int ig = 0; ig < ncouls; ++ig)
-    indinv(ig) = ig;
-  indinv(ncouls) = ncouls - 1;
+    end = system_clock::now();
+    elapsed = end - start;
 
-  for (int iw = nstart; iw < nend; ++iw) {
-    wx_array(iw) = e_lk - e_n1kq + dw * ((iw + 1) - 2);
-    if (wx_array(iw) < to1)
-      wx_array(iw) = to1;
+    std::cout << "********** Kernel Time Taken **********= "
+              << elapsedKernelTimer << " secs"
+              << "\n";
+    std::cout << "********** Total Time Taken **********= " << elapsed.count()
+              << " secs"
+              << "\n";
   }
-
-#else
-  init_structs(number_bands, ngpown, ncouls, aqsmtemp, aqsntemp, I_eps_array,
-               wtilde_array, vcoul, inv_igp_index, indinv, wx_array);
-#endif // Initailize OpenACC structures on the device directily
-
-  k_start = system_clock::now();
-  noflagOCC_solver(number_bands, ngpown, ncouls, inv_igp_index, indinv,
-                   wx_array, wtilde_array, aqsmtemp, aqsntemp, I_eps_array,
-                   vcoul, achtemp);
-
-  k_end = system_clock::now();
-  duration<double> elapsed = k_end - k_start;
-  elapsedKernelTimer = elapsed.count();
-
-  // Check for correctness
-  correntess(achtemp(0));
-  printf("\n Final achtemp\n");
-  achtemp(0).print();
-
-  end = system_clock::now();
-  elapsed = end - start;
-
-  cout << "********** Kernel Time Taken **********= " << elapsedKernelTimer
-       << " secs" << endl;
-  cout << "********** Total Time Taken **********= " << elapsed.count()
-       << " secs" << endl;
-
+  Kokkos::finalize();
   return 0;
 }
 
@@ -309,94 +210,64 @@ void noflagOCC_solver(size_t number_bands, size_t ngpown, size_t ncouls,
                       ARRAY1D_DataType &wx_array, ARRAY2D &wtilde_array,
                       ARRAY2D &aqsmtemp, ARRAY2D &aqsntemp,
                       ARRAY2D &I_eps_array, ARRAY1D_DataType &vcoul,
-                      ARRAY1D &achtemp) {
+                      ComplexStruct &achtemp) {
   time_point<system_clock> start, end;
   start = system_clock::now();
-  // Vars to use for reduction
-  DataType ach_re0 = 0.00, ach_re1 = 0.00, ach_re2 = 0.00, ach_im0 = 0.00,
-           ach_im1 = 0.00, ach_im2 = 0.00;
 
-#if defined(OPENMP_TARGET)
-#pragma omp target enter data map(to                                           \
-                                  : aqsmtemp, vcoul, inv_igp_index, indinv,    \
-                                    aqsntemp, I_eps_array, wx_array,           \
-                                    wtilde_array)
+  // Deep copies between host views to device views of the DualViews.
+  Kokkos::deep_copy(inv_igp_index.d_view, inv_igp_index.h_view);
+  Kokkos::deep_copy(indinv.d_view, indinv.h_view);
+  Kokkos::deep_copy(vcoul.d_view, vcoul.h_view);
+  Kokkos::deep_copy(wx_array.d_view, wx_array.h_view);
+  Kokkos::deep_copy(aqsmtemp.d_view, aqsmtemp.h_view);
+  Kokkos::deep_copy(aqsntemp.d_view, aqsntemp.h_view);
+  Kokkos::deep_copy(I_eps_array.d_view, I_eps_array.h_view);
+  Kokkos::deep_copy(wtilde_array.d_view, wtilde_array.h_view);
 
-  start = system_clock::now();
-
-#pragma omp target map(                                                        \
-    to                                                                         \
-    : aqsmtemp.dptr [0:aqsmtemp.size], vcoul.dptr [0:vcoul.size],              \
-      inv_igp_index.dptr [0:inv_igp_index.size], indinv.dptr [0:indinv.size],  \
-      aqsntemp.dptr [0:aqsmtemp.size], I_eps_array.dptr [0:I_eps_array.size],  \
-      wx_array.dptr [nstart:nend], wtilde_array.dptr [0:wtilde_array.size])    \
-    map(tofrom                                                                 \
-        : ach_re0, ach_re1, ach_re2, ach_im0, ach_im1, ach_im2)
-
-#pragma omp teams distribute parallel for collapse(2) \
-    reduction(+:ach_re0, ach_re1, ach_re2, ach_im0, ach_im1, ach_im2) \
-  num_teams(ngpown*ncouls) thread_limit(32)
-  for (int my_igp = 0; my_igp < ngpown; ++my_igp) {
-    for (int ig = 0; ig < ncouls; ++ig) {
-      for (int n1 = 0; n1 < number_bands; ++n1) {
-
-#elif defined(_OPENMP)
-#pragma omp parallel for collapse(2) \
-    reduction(+:ach_re0, ach_re1, ach_re2, ach_im0, ach_im1, ach_im2)
-  for (int n1 = 0; n1 < number_bands; ++n1) {
-    for (int my_igp = 0; my_igp < ngpown; ++my_igp) {
-      for (int ig = 0; ig < ncouls; ++ig) {
-#elif defined(_OPENACC)
-#pragma acc parallel loop gang vector collapse(3) \
-    present(inv_igp_index, indinv, aqsmtemp, aqsntemp, wtilde_array, wx_array, I_eps_array, vcoul) \
-    reduction(+:ach_re0, ach_re1, ach_re2, ach_im0, ach_im1, ach_im2)\
-    num_gangs(number_bands*ncouls)
-  for (int n1 = 0; n1 < number_bands; ++n1) {
-    for (int my_igp = 0; my_igp < ngpown; ++my_igp) {
-      for (int ig = 0; ig < ncouls; ++ig) {
+  // An MDRangePolicy for 3 nested loops.
+  using md_policy =
+      Kokkos::Experimental::MDRangePolicy<ExecSpace,
+                                          Kokkos::Experimental::Rank<3>, int>;
+#if defined(KOKKOS_ENABLE_CUDA)
+  md_policy iteration({0, 0, 0}, {ncouls, number_bands, ngpown}, {8, 8, 8});
 #else
-  for (int n1 = 0; n1 < number_bands; ++n1) {
-    for (int my_igp = 0; my_igp < ngpown; ++my_igp) {
-      for (int ig = 0; ig < ncouls; ++ig) {
+  md_policy iteration({0, 0, 0}, {number_bands, ngpown, ncouls}, {0, 0, 0});
 #endif
-        int indigp = inv_igp_index(my_igp);
-        int igp = indinv(indigp);
-        DataType achtemp_re_loc[nend - nstart], achtemp_im_loc[nend - nstart];
-        for (int iw = nstart; iw < nend; ++iw) {
-          achtemp_re_loc[iw] = 0.00;
-          achtemp_im_loc[iw] = 0.00;
-        }
-        CustomComplex<DataType> sch_store1 =
-            CustomComplex_conj(aqsmtemp(n1, igp)) * aqsntemp(n1, igp) * 0.5 *
-            vcoul(igp) * wtilde_array(my_igp, igp);
+
+#if defined(KOKKOS_ENABLE_CUDA)
+  Kokkos::parallel_reduce(
+      "noflagOCC_solver", iteration,
+      KOKKOS_LAMBDA(const int ig, const int n1, const int my_igp,
+                    ComplexStruct &update) {
+#else
+  Kokkos::parallel_reduce(
+      "noflagOCC_solver", iteration,
+      KOKKOS_LAMBDA(const int n1, const int my_igp, const int ig,
+                    ComplexStruct &update) {
+#endif
+        int indigp = inv_igp_index.d_view(my_igp);
+        int igp = indinv.d_view(indigp);
+        KokkosComplex aqsmtemp_conj = KokkosComplex(
+            aqsmtemp.d_view(n1, igp).real(), -aqsmtemp.d_view(n1, igp).imag());
+        KokkosComplex sch_store1 = aqsmtemp_conj * aqsntemp.d_view(n1, igp) *
+                                   0.5 * vcoul.d_view(igp) *
+                                   wtilde_array.d_view(my_igp, igp);
 
         for (int iw = nstart; iw < nend; ++iw) {
-          CustomComplex<DataType> wdiff =
-              wx_array(iw) - wtilde_array(my_igp, ig);
-          CustomComplex<DataType> delw =
-              CustomComplex_conj(wdiff) *
-              (1 / CustomComplex_real((wdiff * CustomComplex_conj(wdiff))));
-          CustomComplex<DataType> sch_array =
-              delw * I_eps_array(my_igp, ig) * sch_store1;
+          KokkosComplex wdiff =
+              wx_array.d_view(iw) - wtilde_array.d_view(my_igp, ig);
+          KokkosComplex wdiff_conj = KokkosComplex(wdiff.real(), -wdiff.imag());
+          KokkosComplex delw = wdiff_conj * (1 / (wdiff * wdiff_conj).real());
+          KokkosComplex sch_array =
+              delw * I_eps_array.d_view(my_igp, ig) * sch_store1;
 
-          achtemp_re_loc[iw] += CustomComplex_real(sch_array);
-          achtemp_im_loc[iw] += CustomComplex_imag(sch_array);
+          update.val[iw].real() += sch_array.real();
+          update.val[iw].imag() += sch_array.imag();
         }
-        ach_re0 += achtemp_re_loc[0];
-        ach_re1 += achtemp_re_loc[1];
-        ach_re2 += achtemp_re_loc[2];
-        ach_im0 += achtemp_im_loc[0];
-        ach_im1 += achtemp_im_loc[1];
-        ach_im2 += achtemp_im_loc[2];
-      }
-    } // ngpown
-  }   // number_bands
+      },
+      achtemp); // number_bands
 
   end = system_clock::now();
   duration<double> elapsed = end - start;
   double elapsedKernelTimer = elapsed.count();
-
-  achtemp(0) = CustomComplex<DataType>(ach_re0, ach_im0);
-  achtemp(1) = CustomComplex<DataType>(ach_re1, ach_im1);
-  achtemp(2) = CustomComplex<DataType>(ach_re2, ach_im2);
 }
--- common/Defines.h
+++ common/Defines.h
@@ -1,9 +1,8 @@
 /*
  * Defines a common set of headers and typdefs used in both GPP and FF mini-apps
  * */
-#include "../ComplexClass/CustomComplex.h"
-#include "../arrayMD/arrayMDcpu.h"
-#include "../arrayMD/arrayMD.h"
+#include <Kokkos_Core.hpp>
+#include <Kokkos_DualView.hpp>
 #include <chrono>
 #include <thread>
 
@@ -20,39 +19,56 @@
 using DataType = double;
 using namespace std::chrono;
 
+using KokkosComplex = Kokkos::complex<DataType>;
+using ExecSpace = Kokkos::DefaultExecutionSpace;
+using HostExecSpace = Kokkos::DefaultHostExecutionSpace;
+using Layout = ExecSpace::array_layout;
+using MemSpace = ExecSpace::memory_space;
+
 // ArrayMD definitions
-#define ARRAY3D ArrayMD<CustomComplex<DataType>, 3, Device::cpu>
-#define ARRAY2D ArrayMD<CustomComplex<DataType>, 2, Device::cpu>
-#define ARRAY1D ArrayMD<CustomComplex<DataType>, 1, Device::cpu>
-#define ARRAY1D_int ArrayMD<int, 1, Device::cpu>
-#define ARRAY1D_DataType ArrayMD<DataType, 1, Device::cpu>
+#define ARRAY3D Kokkos::View<KokkosComplex ***, Layout, MemSpace>
+#define ARRAY2D Kokkos::DualView<KokkosComplex **, Layout, MemSpace>
+#define ARRAY1D Kokkos::DualView<KokkosComplex *, Layout, MemSpace>
+#define ARRAY1D_DataType Kokkos::DualView<DataType *, Layout, MemSpace>
+#define ARRAY1D_int Kokkos::DualView<int *, Layout, MemSpace>
+
+// Structure to store nend-nstart number of complex-numbers and define a
+// reduction on those elements.
+struct ComplexStruct {
+  KokkosComplex val[nend - nstart];
+
+  KOKKOS_INLINE_FUNCTION
+  void operator+=(ComplexStruct const &other) {
+    for (int i = 0; i < 3; ++i)
+      val[i] += other.val[i];
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  void operator+=(ComplexStruct const volatile &other) volatile {
+    for (int i = 0; i < 3; ++i)
+      val[i] += other.val[i];
+  }
+};
 
 // Function Definitions
 
-void schDttt_corKernel1(CustomComplex<double> &schDttt_cor,
-                        ARRAY1D_int &inv_igp_index, ARRAY1D_int &indinv,
-                        ARRAY3D &I_epsR_array, ARRAY3D &I_epsA_array,
-                        ARRAY2D &aqsmtemp, ARRAY2D &aqsntemp,
-                        ARRAY1D_DataType &vcoul, int ncouls, int ifreq,
-                        int ngpown, int n1, double fact1, double fact2);
+void schDttt_corKernel1(KokkosComplex &schDttt_cor, ARRAY1D_int &inv_igp_index,
+                        ARRAY1D_int &indinv, ARRAY3D &I_epsR_array,
+                        ARRAY3D &I_epsA_array, ARRAY2D &aqsmtemp,
+                        ARRAY2D &aqsntemp, ARRAY1D_DataType &vcoul, int ncouls,
+                        int ifreq, int ngpown, int n1, double fact1,
+                        double fact2);
 
-void schDttt_corKernel2(CustomComplex<double> &schDttt_cor,
-                        ARRAY1D_int &inv_igp_index, ARRAY1D_int &indinv,
-                        ARRAY3D &I_epsR_array, ARRAY3D &I_epsA_array,
-                        ARRAY2D &aqsmtemp, ARRAY2D &aqsntemp,
-                        ARRAY1D_DataType &vcoul, int ncouls, int ifreq,
-                        int ngpown, int n1, double fact1, double fact2);
+void schDttt_corKernel2(KokkosComplex &schDttt_cor, ARRAY1D_int &inv_igp_index,
+                        ARRAY1D_int &indinv, ARRAY3D &I_epsR_array,
+                        ARRAY3D &I_epsA_array, ARRAY2D &aqsmtemp,
+                        ARRAY2D &aqsntemp, ARRAY1D_DataType &vcoul, int ncouls,
+                        int ifreq, int ngpown, int n1, double fact1,
+                        double fact2);
 
-void
-noflagOCC_solver(size_t number_bands,
-                 size_t ngpown,
-                 size_t ncouls,
-                 ARRAY1D_int& inv_igp_index,
-                 ARRAY1D_int& indinv,
-                 ARRAY1D_DataType& wx_array,
-                 ARRAY2D& wtilde_array,
-                 ARRAY2D& aqsmtemp,
-                 ARRAY2D& aqsntemp,
-                 ARRAY2D& I_eps_array,
-                 ARRAY1D_DataType& vcoul,
-                 ARRAY1D& achtemp);
+void noflagOCC_solver(size_t number_bands, size_t ngpown, size_t ncouls,
+                      ARRAY1D_int &inv_igp_index, ARRAY1D_int &indinv,
+                      ARRAY1D_DataType &wx_array, ARRAY2D &wtilde_array,
+                      ARRAY2D &aqsmtemp, ARRAY2D &aqsntemp,
+                      ARRAY2D &I_eps_array, ARRAY1D_DataType &vcoul,
+                      ComplexStruct &achtemp);
